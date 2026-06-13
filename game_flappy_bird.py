1af9a2dc20e1607ef2600d2ef0fb27e86784e28a
mkdirharsh108 <mkdir.harsh108@gmail.com>
2026-06-14 02:35:42 +0530

    updated game

--- game_flappy_bird.py
+++ game_flappy_bird.py
@@ -9,22 +9,20 @@ done = False
 
 # Initialize PyGame keyboard
 pygame.init()
-screen = pygame.display.get_surface()
+screen = pygame.display.get_surface()  # Gym has already created a window
 
 while not done:
-    action = 0 # default -> 0 is no flap & 1 is flap
+    action = 0  # default -> 0 is no flap & 1 is flap
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYDOWN:  
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                action = 1 # flap
+                action = 1  # flap
 
     state, reward, done, truncated, info = env.step(action)
-    env. render()
+    env.render()
 
 env.close()
 pygame.quit()
-
-# Gym has already created a window
